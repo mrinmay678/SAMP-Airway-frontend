ef90705ae7d29f805f10af4438066e36e8e063c1
aniruddha-21 <aniruddha.nath21@gmail.com>
2022-03-26 11:40:58 +0530

    table?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { LoginComponent } from './login-component/login.component';
 
 const routes: Routes = [
   {
-    component:LoginComponent,
-    path:'login-component'
+    path:'login-component',
+    component:LoginComponent
+
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  headers=["Flight name   ","Source   ","Destination   ","Time of Departure   ","Cost   "];
+  rows=[
+    {"Flight name":'SpiceJet',"Source":'Kolkata',"Destination":'Delhi',"Time of Departure":'11:00',"Cost":'Rs 3999'},
+    {"Flight name":'Air India',"Source":'Kolkata',"Destination":'Mumbai',"Time of Departure":'13:00',"Cost":'Rs 4399'},
+    {"Flight name":'Indigo',"Source":'Kolkata',"Destination":'Hyderabad',"Time of Departure":'09:00',"Cost":'Rs 2799'},
+    {"Flight name":'Air Asia',"Source":'Kolkata',"Destination":'Bangalore',"Time of Departure":'16:00',"Cost":'Rs 4999'},
+    {"Flight name":'Vistara',"Source":'Kolkata',"Destination":'Delhi',"Time of Departure":'10:00',"Cost":'Rs 5399'}
+  ]
+
+  constructor(private router:Router){}
+
+  goToPage(pageName:any) :void
+  {
+    this.router.navigate([`${pageName}`]);
+  }
 }
