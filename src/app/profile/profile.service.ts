6fa8ff065e34270e3693ef8b630b3f1f6cc2f78f
aniruddha-21 <aniruddha.nath21@gmail.com>
2022-03-28 15:53:52 +0530

    table dekhacchena bhai

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getProfile(): Observable<any> {
+    return this.http.get<any>('https://2c3dcdb8-7454-4bd3-8561-a8740a5b1927.mock.pstmn.io/profile');
+  }
 }
