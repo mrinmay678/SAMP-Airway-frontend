ffab01e6e32e447f55dbeab441ffc6d995602a29
mrinmay678 <mrinmay678@gmail.com>
2022-03-28 17:14:51 +0530

    .

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,16 +10,15 @@ export class AuthService {
   constructor(private http:HttpClient) { }
 
   public login(data:any): Observable<any> {
-    return this.http.post<any>('https://6dd470be-7e68-43a2-8261-c9b8975e6c52.mock.pstmn.io/auth/login', data);
+    return this.http.post<any>('https://mock-server-samp.herokuapp.com/login', data);
   }
 
   public refreshToken(data:any): Observable<any> {
-    return this.http.post<any>('https://6dd470be-7e68-43a2-8261-c9b8975e6c52.mock.pstmn.io/auth/refresh-token', data);
+    return this.http.post<any>('https://mock-server-samp.herokuapp.com/refresh-token', data);
   }
 
   public signup(data:any): Observable<any> {
-    
-    return this.http.post<any>('https://6dd470be-7e68-43a2-8261-c9b8975e6c52.mock.pstmn.io/auth/signup', data);
+    return this.http.post<any>('https://mock-server-samp.herokuapp.com/signup', data);
   }
 
 
